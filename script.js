4284fe8c8071e2a0e6b46c98bbac7a6049bc8d1e
mohammed-abdul-rasheed-khalid <rasheedkhalid333@gmail.com>
2024-05-25 07:26:26 +0530

    background changed

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const quoteText = document.querySelector(".quote"),
-quoteBtn = document.querySelector("button"),
-authorName = document.querySelector(".author .name"),
-soundBtn = document.querySelector(".sound"),
-copyBtn = document.querySelector(".copy"),
-twitterBtn = document.querySelector(".twitter");
+    quoteBtn = document.querySelector("button"),
+    authorName = document.querySelector(".author .name"),
+    soundBtn = document.querySelector(".sound"),
+    copyBtn = document.querySelector(".copy"),
+    twitterBtn = document.querySelector(".twitter");
 
 //random quote function
-function randomQuote(){
+function randomQuote() {
     quoteBtn.classList.add("loading");
     quoteBtn.innerText = "Loading Quote ... ";
     //fetching random quotes from api and parsing it into the function
-    fetch("https://api.quotable.io/random").then(res => res.json()).then( result =>{
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
         quoteText.innerText = result.content;
         authorName.innerText = result.author;
         quoteBtn.innerText = "New Quote";
@@ -18,18 +18,18 @@ function randomQuote(){
     });
 }
 
-soundBtn.addEventListener("click",()=>{
+soundBtn.addEventListener("click", () => {
     let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
     speechSynthesis.speak(utterance);
 })
 
-copyBtn.addEventListener("click",()=>{
+copyBtn.addEventListener("click", () => {
     navigator.clipboard.writeText(quoteText.innerText)
 })
 
-twitterBtn.addEventListener("click",()=>{
+twitterBtn.addEventListener("click", () => {
     let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
-    window.open(tweetUrl,"__blank");
+    window.open(tweetUrl, "__blank");
 })
 
-quoteBtn.addEventListener("click",randomQuote);
+quoteBtn.addEventListener("click", randomQuote);
